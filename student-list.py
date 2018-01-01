bd8bcb15b583fe3c4ec3e109ed07a89ba13c98a1
Zhikrullah <abuhanifah001@gmail.com>
2018-01-01 03:14:38 +0100

    made second change to file

--- student-list.py
+++ student-list.py
@@ -2,7 +2,7 @@ import random
 import sys
 import os
 import datetime
-from Person import *
+
 
 class StudentList:
     try:
@@ -11,7 +11,6 @@ class StudentList:
         __name = None
         __count = 1
         __age = 0
-        myage = Person()
 
         def __init__(self):
             pass
@@ -19,15 +18,14 @@ class StudentList:
         def eligiblelist(self,no):
             self.__no = no
 
+            while (len(self.__studentList) < self.__no):
+                self.__name = input("Enter student name {}: ".format(self.__count))
+                self.__studentList.append(self.__name)
+                self.__count += 1
             if (self.__no == 0):
                 print("No Student invited")
             else:
-                while(len(self.__studentList) < self.__no):
-                    self.__name = input("Enter student name {}: ".format(self.__count))
-                    #age = int(input("Enter Age of student: "))
-                    self.__studentList.append(self.__name)
-                    self.__count += 1
-
+               pass
             print("\n:::List of Invited student:::")
             for x in self.__studentList:
                 print(x)
