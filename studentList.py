0fdf61221c669cc7d92519b2ada4fbea491f6375
Zhikrullah <abuhanifah001@gmail.com>
2018-01-02 15:52:16 +0100

    First commit for ammended file

--- studentList.py
+++ studentList.py
@@ -2,50 +2,39 @@ import random
 import sys
 import os
 import datetime
-
+from Person import *
 
 class StudentList:
-
-
     try:
         __studentList = []
         __no = 0
         __name = None
         __count = 1
-        finename = None
 
-        def __init__(self):
-            pass
 
         def eligiblelist(self,no):
             self.__no = no
 
             while (len(self.__studentList) < self.__no):
                 self.__name = input("Enter student name {}: ".format(self.__count))
-                if self.__name in self.__studentList:
-                    print("Name already exist. Please add another name not in the list \n!!!!See list Below!!!!")
-                    print(self.__studentList)
-                    continue
-                elif (self.__no == 0):
+                if(self.__name not in self.__studentList):
+                    self.__studentList.append(self.__name)
+                elif(self.__no == 0):
                     print("No Student invited")
                 else:
-                    self.__studentList.append(self.__name)
-
+                    print("::: Name already exist in the list please add another name. \n See list below :::")
+                    print(self.__studentList)
+                    continue
                 self.__count += 1
+
+
 
             print("\n:::List of Invited student:::")
             for x in self.__studentList:
                 print(x)
 
-        def savelisttofile(self):
-            with open("student.csv","w") as file:
-                for x in self.__studentList:
-                    file.write(x)
 
-        def readfile(self,filename):
-            with open(filename,"r") as file:
-                for x in file:
-                    print(x)
+
     except:
         error = sys.exc_info()[0]
         print("Error: {}".format(error))
@@ -54,14 +43,10 @@ class StudentList:
 student = StudentList()
 
 try:
-    num_student = int(input("Enter number of student invited: "))
-    student.eligiblelist(num_student)
-    student.savelisttofile()
-    student.readfile('student.csv')
-
+    no_student = int(input("Enter number of student invited: "))
+    student.eligiblelist(no_student)
 except ValueError:
     print("Invalid value entered\nPlease enter the correct value")
-
 except:
     error = sys.exc_info()[0]
     print("Error Detected: {}".format(error))
